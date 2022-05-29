4b3e2e7a022710d95a9863c6640d2ea7b142cc86
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-05-30 02:52:03 +0530

    app

--- src/components/Cryp.jsx
+++ src/components/Cryp.jsx
@@ -6,9 +6,9 @@ import { Box,Image} from "@chakra-ui/react";
 import logo from './cryptoanal.png'
 export default function Cryp() {
   const [price, setPrice] = useState([]);
-  const [cur, setCur] = useState("usd");
+  const [cur, setCur] = useState("USD");
   const [top, setTop] = useState(10);
-
+  const [sym, setSym] = useState("$");
   const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${cur}&order=market_cap_desc&per_page=${top}&page=1&sparkline=false`;
   useEffect(() => {
     axios.get(url).then((res) => {
@@ -21,30 +21,64 @@ export default function Cryp() {
     setTop(e.target.value);
     console.log(e.target.value);
   };
- 
+  const change = (e) => {
+    // setCur(e.target.value)
+    setCur(e.target.value);
+    switch (e.target.value) {
+      case "USD":
+        setSym("$");
+        break;
+      case "INR":
+        setSym("₹");
+        break;
+      case "EUR":
+        setSym("€");
+        break;
+      case "JPY":
+        setSym("¥");
+        break;
+      default:
+        setSym(`${e.target.value} `);
+        break;
+    }
+  };
+
   return (
-    <Box height="110%" backgroundColor="gray.700">
+    <Box height="100%" width='100vw' backgroundColor="gray.700" overflow='hidden'>
      <HStack height='10%' backgroundColor='gray.900' mb='8'  width='100%' position='fixed' zIndex='2'>
          
-         <Image pl='4vw' pb='1vh' src={logo} height='75%'></Image>
+         <Image pl={['0vw','4vw']} pb='1vh' src={logo} height={['98%','95%']} pr={["6vw","50vw"]}></Image>
          
-     <Select variant='flushed' onChange={changeTop} width='25vw' value={top}  color='gray.500' placeholder='Select option'>
+     <Select variant='flushed' onChange={changeTop} width={['25vw','15vw']} value={top}  color='gray.500' placeholder='Select option'>
   <option bg='red' value='10'>Top 10</option>
   <option value='50'>Top 50</option>
   <option value='100'>Top 100</option>
   <option value='150'>Top 150</option>
   <option value='250'>Top 250</option>
 </Select>
+<Select variant='flushed' onChange={change} width={['25vw','15vw']} value={cur}  color='gray.500' placeholder='Select option'>
+  <option bg='red' value='USD'>USD</option>
+  <option value='INR'>INR</option>
+  <option value='EUR'>EUR</option>
+  <option value='JPY'>JPY YEN</option>
+  <option value='BTC'>BTC</option>
+  <option value='LTC'>LTC</option>
+  <option value='ETH'>ETH</option>
+  <option value='MATIC'>MATIC</option>
+  <option value='SOL'>SOL</option>
+  <option value='ADA'>ADA</option>
+  <option value='DOT'>DOT</option>
+</Select>
      </HStack>
 
-      <Box height='90%' pt='6%' pb='2%' align='center'>
-        <HStack align='center' width='90%' height='10' bg='gray.900'>
-            <Box ml='5%' color='white'>Rank</Box>
+      <Box height='90%' pt={['20%','6%']} pb='2%' align='center'>
+        <HStack align='center' width={['100%','90%']} height='10' bg='gray.900'>
+            <Box ml='5%' display={["none", "", "", "block"]} color='white'>Rank</Box>
             <Box pl='5%' color='white'>Name</Box>
-            <Box pl='18%' color='white'>Current Price</Box>
-            <Box pl='3%' color='white'>24h %</Box>
-            <Box pl='5%' color='white'>Market Cap</Box>
-            <Box pl='7%' color='white'>Circulating Supply</Box>
+            <Box pl={['25%','18%']} color='white'>Current Price</Box>
+            <Box pl={['8%','3%']} color='white'>24h %</Box>
+            <Box pl='5%' display={["none", "", "", "block"]} color='white'>Market Cap</Box>
+            <Box pl='7%' display={["none", "", "", "block"]} color='white'>Circulating Supply</Box>
         </HStack>
         {price.map((p) => (
           <Box>
@@ -59,6 +93,7 @@ export default function Cryp() {
               cirSup={p.circulating_supply}
               totSup={p.total_supply}
               maxSup={p.max_supply}
+              cur={sym}
             />
           </Box>
         ))}
--- src/components/Patch.jsx
+++ src/components/Patch.jsx
@@ -4,45 +4,23 @@ export default function Patch(props) {
   const d = props.dayH;
   const [sym, setSym] = useState("$");
   const [cur, setCur] = useState("usd");
-  const change = (e) => {
-    // setCur(e.target.value)
-    setCur(e.target.value);
-    switch (e.target.value) {
-      case "usd":
-        setSym("$");
-        break;
-      case "inr":
-        setSym("₹");
-        break;
-      case "eur":
-        setSym("€");
-        break;
-      case "jpy":
-        setSym("¥");
-        break;
-      default:
-        setSym(`${e.target.value} `);
-        break;
-    }
-  };
-
+  
   return (
-    <Box align="center">
+    <Box align="center" width='100vw'>
       <Box
         width={["100%", "90%"]}
         borderRadius="lg"
         backgroundColor="gray.900"
         color="gray.100"
       >
-        <Box></Box>
-        <HStack width="90%" mt="2" height="10vh" flexDirection="row" p="2">
-          <Box ml="-5" width="16" align="center" mr="4">
+        <HStack width={["100%","90%"]} mt="2" height="10vh" flexDirection="row" p={["","2"]}>
+          <Box ml="-5" width={["8","16"]} align="center" mr="4">
             {props.rank}
           </Box>
-          <HStack width="18vw" position="relative">
+          <HStack fontSize={["15",""]} width={["55vw","18vw"]} >
             <Image
               borderRadius="full"
-              boxSize="3.5vh"
+              boxSize={["2vh","3.5vh"]}
               src={props.image}
             ></Image>
             <Box>{props.name}</Box>
@@ -50,31 +28,35 @@ export default function Patch(props) {
               {props.symb}
             </Box>
           </HStack>
-          <Box align='right' width="9vw">
-            {sym} {props.price.toFixed(3)}
+          <Box align={["left","right"]} width={["55vw","9vw"]}>
+            {props.cur} {props.price.toFixed(3)}
           </Box>
 
           {/* <Box width='7vw' color='green'>{(100*props.dayh/props.price).toFixed(2)}</Box> */}
           <Box
-          align='right'
-            width="6vw"
+            align="right"
+            width={["15vw","6vw"]}
             fontWeight="semibold"
-            color={d>0?'green.300':'red'}
+            color={d > 0 ? "green.300" : "red"}
           >{`${d.toFixed(2)}%`}</Box>
-          <Box width="9.5vw" align='right'>
+          <Box display={["none", "", "", "block"]} width="9.5vw" align="right">
             $
             {props.marCap.toLocaleString(undefined, {
               maximumFractionDigits: 3,
             })}
           </Box>
-          <Box width='15.1vw' pl='5vw' align='right'>
-            <Box pb='1'
+          <Box width="15.1vw" pl="5vw" align="right">
+            <Box
+              display={["none", "", "", "block"]}
+              pb="1"
               style={{ textTransform: "uppercase" }}
             >{`${props.cirSup.toLocaleString(undefined, {
               maximumFractionDigits: 0,
             })} ${props.symb}`}</Box>
             {props.maxSup !== null && (
-              <Box overflow='hidden'
+              <Box
+                display={["none", "", "", "block"]}
+                overflow="hidden"
                 align="left"
                 position="absolute"
                 borderRadius="full"
@@ -86,7 +68,7 @@ export default function Patch(props) {
                   position="absolute"
                   borderRadius="full"
                   backgroundColor="gray.300"
-                  width={`${(props.totSup/props.cirSup)*80}%`} 
+                  width={`${(props.totSup / props.cirSup) * 80}%`}
                   height="1"
                 ></Box>
               </Box>
