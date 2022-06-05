a4ee4823e54bd2c4644eec5de16e1cbdc833eadb
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-06-05 19:51:44 +0530

    updates

--- src/components/Cryp.jsx
+++ src/components/Cryp.jsx
@@ -1,31 +1,31 @@
 import React, { useEffect, useState } from "react";
-import {HStack,Select} from "@chakra-ui/react";
+import { HStack, Select } from "@chakra-ui/react";
 import axios from "axios";
 import Patch from "./Patch";
-import { Box,Image} from "@chakra-ui/react";
-import logo from './cryptoanal.png'
-import loading from './loading.gif'
+import { Box, Image} from "@chakra-ui/react";
+import logo from "./cryptoanal.png";
+import loading from "./loading.gif";
 export default function Cryp() {
   const [price, setPrice] = useState([]);
   const [cur, setCur] = useState("USD");
   const [top, setTop] = useState(10);
   const [sym, setSym] = useState("$");
-  const [load,setLoad]=useState(true)
+  const [load, setLoad] = useState(true);
   const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${cur}&order=market_cap_desc&per_page=${top}&page=1&sparkline=false`;
   useEffect(() => {
     axios.get(url).then((res) => {
       setPrice(res.data);
-      setLoad(false)
+      setLoad(false);
     });
   }, [url]);
 
   const changeTop = (e) => {
     setTop(e.target.value);
-    e.preventDefault()
+    e.preventDefault();
   };
   const change = (e) => {
     setCur(e.target.value);
-    e.preventDefault()
+    e.preventDefault();
     switch (e.target.value) {
       case "USD":
         setSym("$");
@@ -44,65 +44,118 @@ export default function Cryp() {
         break;
     }
   };
-
   return (
-    <Box height="100%" width='100vw' backgroundColor="gray.700" overflow='hidden'>
-     <HStack height={['8vh','10vh']} backgroundColor='gray.900'  width='100%' position='fixed' zIndex='2'>
-         
-         <Image pl={['0vw','4vw']} mb='1vh' src={logo} height={['80%','95%']} pr={["6vw","50vw"]}></Image>
-         
-     <Select variant='flushed' onChange={changeTop} width={['25vw','15vw']} value={top}  color='gray.500' placeholder='Select option'>
-  <option bg='red' value='10'>Top 10</option>
-  <option value='50'>Top 50</option>
-  <option value='100'>Top 100</option>
-  <option value='150'>Top 150</option>
-  <option value='250'>Top 250</option>
-</Select>
-<Select variant='flushed' onChange={change} width={['25vw','15vw']} value={cur}  color='gray.500' placeholder='Select option'>
-  <option bg='red' value='USD'>USD</option>
-  <option value='INR'>INR</option>
-  <option value='EUR'>EUR</option>
-  <option value='JPY'>JPY YEN</option>
-  <option value='BTC'>BTC</option>
-  <option value='LTC'>LTC</option>
-  <option value='ETH'>ETH</option>
-  <option value='MATIC'>MATIC</option>
-  <option value='SOL'>SOL</option>
-  <option value='ADA'>ADA</option>
-  <option value='DOT'>DOT</option>
-</Select>
-     </HStack>
-     {load===true? <Box align='center' height='75vh' mt='25vh'>
-        <Image height={['30vh','50vh']} src={loading}></Image>
-      </Box> :
-      <Box height='90%' pt={['10vh','6%']} pb='2%' align='center'>
-        <HStack align='center' width={['100%','90%']} height='10' bg='gray.900'>
-            <Box ml='5%' display={["none", "", "", "block"]} color='white'>Rank</Box>
-            <Box pl={['9%','5%']} color='white'>Name</Box>
-            <Box pl={['25%','20%']} color='white'>Current Price</Box>
-            <Box pl={['10%','3%']} color='white'>24h %</Box>
-            <Box pl='5%' display={["none", "", "", "block"]} color='white'>Market Cap</Box>
-            <Box pl='7%' display={["none", "", "", "block"]} color='white'>Circulating Supply</Box>
-        </HStack>
-        {price.map((p) => (
-          <Box>
-            <Patch
-              image={p.image}
-              rank={p.market_cap_rank}
-              name={p.name}
-              price={p.current_price}
-              symb={p.symbol}
-              dayH={p.price_change_percentage_24h}
-              marCap={p.market_cap}
-              cirSup={p.circulating_supply}
-              totSup={p.total_supply}
-              maxSup={p.max_supply}
-              cur={sym}
-            />
-          </Box>
-        ))}
-      </Box>
-        }
+    <Box
+      height="100%"
+      width="100vw"
+      backgroundColor="gray.700"
+      overflow="hidden"
+    >
+      <HStack
+        height={["8vh", "10vh"]}
+        backgroundColor="gray.900"
+        width="100%"
+        position="fixed"
+        zIndex="2"
+      >
+        <Image
+          pl={["0vw", "4vw"]}
+          mb="1vh"
+          src={logo}
+          height={["80%", "95%"]}
+          pr={["6vw", "50vw"]}
+        ></Image>
+
+
+        <Select
+          variant="flushed"
+          onChange={changeTop}
+          width={["25vw", "15vw"]}
+          value={top}
+          color="gray.500"
+          placeholder="Select option"
+        >
+          <option bg="red" value="10">
+            Top 10
+          </option>
+          <option value="50">Top 50</option>
+          <option value="100">Top 100</option>
+          <option value="150">Top 150</option>
+          <option value="250">Top 250</option>
+        </Select>
+        <Select
+          variant="flushed"
+          onChange={change}
+          width={["25vw", "15vw"]}
+          value={cur}
+          color="gray.500"
+          placeholder="Select option"
+        >
+          <option bg="red" value="USD">
+            USD
+          </option>
+          <option value="INR">INR</option>
+          <option value="EUR">EUR</option>
+          <option value="JPY">JPY YEN</option>
+          <option value="BTC">BTC</option>
+          <option value="LTC">LTC</option>
+          <option value="ETH">ETH</option>
+          <option value="MATIC">MATIC</option>
+          <option value="SOL">SOL</option>
+          <option value="ADA">ADA</option>
+          <option value="DOT">DOT</option>
+        </Select>
+      </HStack>
+      {load === true ? (
+        <Box align="center" height="75vh" mt="25vh">
+          <Image height={["30vh", "50vh"]} src={loading}></Image>
+        </Box>
+      ) : (
+        <Box height="90%"pt={["10vh", "6%"]} pb="2%" align="center">
+          <HStack
+            align="center"
+            width={["100%", "90%"]}
+            height="10"
+            bg="gray.900"
+          >
+            <Box ml="5%" display={["none", "", "", "block"]} color="white">
+              Rank
+            </Box>
+            <Box pl={["9%", "5%"]} color="white">
+              Name
+            </Box>
+            <Box pl={["25%", "20%"]} color="white">
+              Current Price
+            </Box>
+            <Box pl={["10%", "3%"]} color="white">
+              24h %
+            </Box>
+            <Box pl="5%" display={["none", "", "", "block"]} color="white">
+              Market Cap
+            </Box>
+            <Box pl="8%" display={["none", "", "", "block"]} color="white">
+              Circulating Supply
+            </Box>
+          </HStack>
+          {price.map((p) => (
+            <Box>
+              <Patch
+                image={p.image}
+                rank={p.market_cap_rank}
+                name={p.name}
+                price={p.current_price}
+                symb={p.symbol}
+                dayH={p.price_change_percentage_24h}
+                marCap={p.market_cap}
+                cirSup={p.circulating_supply}
+                totSup={p.total_supply}
+                maxSup={p.max_supply}
+                cur={sym}
+              />
+            </Box>
+          ))}
+        </Box>
+      )}
     </Box>
   );
 }
--- src/components/Patch.jsx
+++ src/components/Patch.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HStack, Box, Image,StatArrow,Stat} from "@chakra-ui/react";
+import { HStack, Box, Image,StatArrow,Stat,Progress} from "@chakra-ui/react";
 export default function Patch(props) {
   const d = props.dayH;
 
@@ -18,13 +18,13 @@ export default function Patch(props) {
           flexDirection="row"
           p={["0", "2"]}
         >
-          <Box ml={["-7%", "0"]} width={["8", "16"]} align="center" mr="4">
+          <Box ml={["-7%", "0"]} width={["8", "16"]} display={["none", "", "", "block"]} align="center" mr="4">
             {props.rank}
           </Box>
           <HStack fontSize={["15", ""]} width={["99vw", "18vw"]}>
             <Image
               borderRadius="full"
-              boxSize={["2vh", "3.5vh"]}
+              boxSize={["2.5vh", "3.5vh"]}
               src={props.image}
             ></Image>
             <Box>{props.name}</Box>
@@ -35,12 +35,12 @@ export default function Patch(props) {
           <Box
             position={["relative", "none"]}
             left={["5%", "0"]}
-            pr={["8%", "0"]}
+            pr={["8%", "2"]}
             pl={["0%", "0"]}
             align="right"
             width={["85vw", "9vw"]}
           >
-            {props.cur} {props.price.toFixed(1)}
+            {props.cur} {props.price.toFixed(2)}
           </Box>
 
           {/* <Box width='7vw' color='green'>{(100*props.dayh/props.price).toFixed(2)}</Box> */}
@@ -57,9 +57,9 @@ export default function Patch(props) {
           <StatArrow type={d>0?"increase":"decrease"}></StatArrow>
           </Stat>
             <Box>{`${d.toFixed(2)}%`}</Box></HStack>
-          <Box
+          <Box 
             display={["none", "", "", "block"]}
-            width={["0", "9.5vw"]}
+            width={["0", "11vw"]}
             align="right"
           >
             {props.cur}
@@ -67,7 +67,8 @@ export default function Patch(props) {
               maximumFractionDigits: 3,
             })}
           </Box>
-          <Box width="15.1vw" pl="5vw" align="right">
+          <Box width="16vw" pl="5vw" align="right">
+            
             <Box
               display={["none", "", "", "block"]}
               pb="1"
@@ -76,24 +77,25 @@ export default function Patch(props) {
               maximumFractionDigits: 0,
             })} ${props.symb}`}</Box>
             {props.maxSup !== null && (
-              <Box
-                display={["none", "", "", "block"]}
-                overflow="hidden"
-                align="left"
-                position="absolute"
-                borderRadius="full"
-                backgroundColor="gray.600"
-                width="10vw"
-                height="1"
-              >
-                <Box
-                  position="absolute"
-                  borderRadius="full"
-                  backgroundColor="gray.300"
-                  width={`${(props.totSup / props.cirSup) * 80}%`}
-                  height="1"
-                ></Box>
-              </Box>
+              <Progress width='85%' display={["none", "", "", "block"]} value={(props.totSup/props.cirSup)*10} size='xs' borderRadius='50' colorScheme='red'/>
+              // <Box
+              //   display={["none", "", "", "block"]}
+              //   overflow="hidden"
+              //   align="left"
+              //   position="absolute"
+              //   borderRadius="full"
+              //   backgroundColor="gray.600"
+              //   width="10vw"
+              //   height="1"
+              // >
+              //   <Box
+              //     position="absolute"
+              //     borderRadius="full"
+              //     backgroundColor="gray.300"
+              //     width={`${(props.totSup / props.cirSup) * 80}%`}
+              //     height="1"
+              //   ></Box>
+              // </Box>
             )}
           </Box>
         </HStack>
